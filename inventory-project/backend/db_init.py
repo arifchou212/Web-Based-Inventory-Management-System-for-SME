eccaed71c72c717891db712f062d9b75ea2f7df7
arifchou212 <116723589+arifchou212@users.noreply.github.com>
2026-08-16 19:31:51 +0100

    Update Firebase initialization to use environment variable

--- inventory-project/backend/db_init.py
+++ inventory-project/backend/db_init.py
@@ -1,8 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+import json
 
-cred = credentials.Certificate("firebase_config.json")
+firebase_creds = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
